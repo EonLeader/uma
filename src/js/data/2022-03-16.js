7c58ce2da1120dcdcd99dca7ea752d0a14e50dae
EonLeader <70076374+EonLeader@users.noreply.github.com>
2022-03-16 21:15:05 +0000

    Add files via upload

---
+++ src/js/data/2022-03-16.js
@@ -0,0 +1,577 @@
+dataSetVersion = "2022-03-16"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series",
+    key: "series",
+    tooltip: "Check this to restrict to certain series.",
+    checked: false,
+    sub: [
+	  { name: "Uma Musume", tooltip: "Uma Musume", key: "UM" },
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Haruna Kawai",
+    img: "ajPXxX2.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ruriko Noguchi",
+    img: "i5QetLQ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Mio Hoshitani",
+    img: "H7GFdYO.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Miyu Yoshizaki",
+    img: "3KeMlIN.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ayaka Fukuhara",
+    img: "2XVLus7.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Sachika Misawa",
+    img: "DjT0VP7.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hinaki Yano",
+    img: "HzRYrNy.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hiyori Nitta",
+    img: "g0XA6Ml.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yurina Amami",
+    img: "L6M4a5w.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Kotomi Aihara",
+    img: "oVb7U3D.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Miho Okazaki",
+    img: "dQvuEZJ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Saya Aizawa",
+    img: "Ujda9gp.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Misaki Watada",
+    img: "Rzcz0RN.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ruriko Aoki",
+    img: "VLdK4Ox.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Eri Suzuki",
+    img: "5LE0QdN.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Minori Suzuki",
+    img: "8GIYT16.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Kikuko Inoue",
+    img: "A3vmZ7m.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yui Ogura",
+    img: "nDyvEwz.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Aimi Tanaka",
+    img: "yke3BaU.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hikari Kubota",
+    img: "fBT8H5g.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ikumi Hasegawa",
+    img: "3IZhJ7t.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Masumi Tazawa",
+    img: "ynSbQ8z.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Sumire Uesaka",
+    img: "ZBLz0N2.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Afumi Hashi",
+    img: "ng6P9x5.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yukina Shuto",
+    img: "cLlRzuC.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Marie Miyake",
+    img: "gxE6HfA.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Nozomi Yamamoto",
+    img: "uSvJ6Os.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Chinami Hashimoto",
+    img: "v2UUWh4.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yui Kondou",
+    img: "3uKwcYI.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Tomomi Mineuchi",
+    img: "fgzm2jw.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Aya Yamane",
+    img: "oTfTxBF.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Manaka Iwami",
+    img: "lFoOIzx.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Keiko Watanabe",
+    img: "8RCLP9R.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Miharu Hanai",
+    img: "9qLDDib.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ai Fairouz",
+    img: "AqRgX11.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yuri Noguchi",
+    img: "J26F4HA.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Emiri Suyama",
+    img: "FWFqqWE.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Akari Kito",
+    img: "L7nCKgz.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Saki Kosaka",
+    img: "RAET4Kf.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ayumi Hinohara",
+    img: "fjdW9cn.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Azusa Tadokoro",
+    img: "Wxr82rG.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Minami Tsuda",
+    img: "MPEiYFD.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ayami Fujino",
+    img: "cZEc2DH.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Megumi Toyoguchi",
+    img: "V6H2154.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Kaori Mizuhashi",
+    img: "Z26cehQ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Shiori Sugiura",
+    img: "Sspzseg.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Kana Yuki",
+    img: "WUnS890.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hitomi Oowada",
+    img: "KRE8dua.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hina Tachibana",
+    img: "medc0ER.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yuuki Takada",
+    img: "5jyFfoo.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yukiyo Fujii",
+    img: "bja6Nq2.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yu Sasahara",
+    img: "vhDBRY0.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hitomi Sasaki",
+    img: "cLfbdCm.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Kaori Maeda",
+    img: "uB1rfZy.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Haruka Terui",
+    img: "CD6Ojo3.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Shino Shimoji",
+    img: "xVcFzb3.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ayaka Imamura",
+    img: "f1lMk3s.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hikaru Tono",
+    img: "v8mZYuC.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Naomi Ozora",
+    img: "t7o898N.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Karin Takahashi",
+    img: "LCnVPsY.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Haruno Inoue",
+    img: "wSF3XKr.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Sora Tokui",
+    img: "VZSfVSO.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Chisa Kimura",
+    img: "bfGvBoL.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yuiko Tatsumi",
+    img: "JkuH41W.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Iori Saeki",
+    img: "ZMmOpuG.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Hitomi Ueda",
+    img: "3MSZpW9.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Rei Matsuzaki",
+    img: "cssdCqv.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Rena Maeda",
+    img: "iVLFqdj.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yui Watanabe",
+    img: "KHseACA.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Yuka Ohtsubo",
+    img: "FfzCtqi.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Minami Takahashi",
+    img: "awF5FNJ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Saori Onishi",
+    img: "GHltAl6.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Rika Kinugawa",
+    img: "qnOSeSQ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Eriko Matsui",
+    img: "vngsKX6.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Ayaka Ohashi",
+    img: "3NbpzsW.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Lynn",
+    img: "niA6zcJ.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Machico",
+    img: "3W18j7L.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Tomoyo Takayanagi",
+    img: "TDiWQ8J.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Marika Kouno",
+    img: "yFQi3V6.png",
+    opts: {
+      series: ["UM"]
+    }
+  },
+  {
+    name: "Azumi Waki",
+    img: "VXD5EhY.png",
+    opts: {
+      series: ["UM"]
+    }
+  }
+];
